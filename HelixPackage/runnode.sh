b8360a893d902a4179e7421005eb617faf72ff14
Praveen <SPraveen@ashleyfurniture.com>
2022-02-16 06:51:59 +0000

    updated code

--- HelixPackage/runnode.sh
+++ HelixPackage/runnode.sh
@@ -1,16 +1,16 @@
 echo " run node sh search for node-red"
 PID=`ps -eaf | grep node-red | grep -v grep | awk '{print $2}'`
-if [ "" !=  "$PID" ]; then
-  echo "killing $PID"
-  echo "killing node-red"
-  kill -9 $PID
-fi
-echo "---------------------------------------"
-echo "Moving to Node-RED directory"
-echo "starting node-red "
-/data/data/com.termux/files/usr/bin/node /data/data/com.termux/files/usr/lib/node_modules/node-red/red.js -s /data/data/com.termux/files/usr/lib/node_modules/node-red/settings.js
-sleep 5
+if [ "" !=  "$PID" ];
+then
+  echo "Node-RED already running with $PID"  
+else
+/data/data/com.termux/files/usr/bin/node /data/data/com.termux/files/usr/lib/node_modules/node-red/red.js -s /data/data/com.termux/files/usr/lib/node_modules/node-red/settings.js &
+sleep 2
+echo "Node-RED not running and restarting"
+
+
 PID2=`ps -eaf | grep node-red | grep -v grep | awk '{print $2}'`
 if [ "" !=  "$PID2" ]; then
   echo "Node-RED started with $PID"
+fi
 fi
